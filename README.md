16c0fee092af9e5b492b2fe28fb9ed59eeb2b69e
sobiemir <sobiemir@aculo.pl>
2019-01-09 19:07:31 +0100

    Add README and change project structure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # roadcross
+
+Kompilacja kodu:
+
+    gcc klient_samochod.c -o klient_samochod -lpthread
+    gcc klient_pieszy.c -o klient_pieszy -lpthread
+    gcc klient_tramwaj.c -o klient_tramwaj -lpthread
+    gcc serwer.c -o serwer -lX11
+
+Uruchomienie:
+
+    ./serwer
+    ./klient_samochod
+    ./klient_pieszy
+    ./klient_tramwaj
--- compile.sh
+++ compile.sh
@@ -10,22 +10,22 @@ if [ ! -d $DIR ]; then
 fi
 
 cd $DIR
-if gcc "../$CLPED.c" -o "$CLPED" -lpthread; then
+if gcc "../src/$CLPED.c" -o "$CLPED" -lpthread; then
 	echo "$CLPED compiled successfully"
 else
 	echo "Compile failed for $CLPED"
 fi
-if gcc "../$CLCAR.c" -o "$CLCAR" -lpthread; then
+if gcc "../src/$CLCAR.c" -o "$CLCAR" -lpthread; then
 	echo "$CLCAR compiled successfully"
 else
 	echo "Compile failed for $CLCAR"
 fi
-if gcc "../$CLTRAM.c" -o "$CLTRAM" -lpthread; then
+if gcc "../src/$CLTRAM.c" -o "$CLTRAM" -lpthread; then
 	echo "$CLTRAM compiled successfully"
 else
 	echo "Compile failed for $CLTRAM"
 fi
-if gcc "../$SERVER.c" -o "$SERVER" -lX11; then
+if gcc "../src/$SERVER.c" -o "$SERVER" -lX11; then
 	echo "$SERVER compiled successfully"
 else
 	echo "Compile failed for $SERVER"
